55d27eebe1c27211689766bb8eb467c32806be40
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2025-03-30 12:52:51 +0700

    Add dummy data.

--- migration-2.sql
+++ migration-2.sql
@@ -10,7 +10,13 @@ CREATE TABLE account (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT,
     email TEXT UNIQUE,
-    password TEXT NULL
+    password TEXT NULL,
     total_post_like NUMERIC DEFAULT 0,
-    total_chat_message NUMERIC DEFAULT 0,
+    total_chat_message NUMERIC DEFAULT 0
 );
+
+-- dml
+INSERT INTO account(id, name, email, password, total_post_like, total_chat_message) VALUES
+('65c13016-05f5-449b-9472-7afca5de2d03'::uuid, 'name0', 'email0@mail.com', 'password0', 0, 0),
+('fb703a0b-9593-4282-bdfc-942131f23182'::uuid, 'name1', 'email1@mail.com', 'password1', 1, 1),
+('2c61386d-0f37-48bb-a247-9cb5927d8974'::uuid, 'name2', 'email2@mail.com', 'password2', 2, 2);
--- migration-3.sql
+++ migration-3.sql
@@ -1,3 +1,4 @@
+-- Active: 1743308173981@@127.0.0.1@5433@db
 -- extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
@@ -26,7 +27,8 @@ CREATE TABLE post_like (
 DROP TABLE IF EXISTS chat_room CASCADE;
 CREATE TABLE chat_room (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    description TEXT NOT NULL
 );
 
 -- Create the chat room member table
@@ -47,3 +49,49 @@ CREATE TABLE chat_message (
     content TEXT NOT NULL,
     FOREIGN KEY (chat_room_id) REFERENCES chat_room(id) ON DELETE CASCADE
 );
+
+-- dml
+INSERT INTO post(account_id, title, content)
+VALUES
+('65c13016-05f5-449b-9472-7afca5de2d03'::uuid, 'title0', 'content0'),
+('fb703a0b-9593-4282-bdfc-942131f23182'::uuid, 'title1', 'content1'),
+('2c61386d-0f37-48bb-a247-9cb5927d8974'::uuid, 'title2', 'content2');
+
+
+INSERT INTO post_like(post_id, account_id) 
+SELECT post.id, account.id
+FROM post
+CROSS JOIN ( 
+    VALUES 
+    ('65c13016-05f5-449b-9472-7afca5de2d03'::uuid), 
+    ('fb703a0b-9593-4282-bdfc-942131f23182'::uuid),
+    ('2c61386d-0f37-48bb-a247-9cb5927d8974'::uuid)
+) AS account(id);
+
+INSERT INTO chat_room(name, description) VALUES
+('name0', 'description0'),
+('name1', 'description1'),
+('name2', 'description2');
+
+INSERT INTO chat_room_member(chat_room_id, account_id)
+SELECT chat_room.id, account.id
+FROM chat_room
+CROSS JOIN ( 
+    VALUES 
+    ('65c13016-05f5-449b-9472-7afca5de2d03'::uuid), 
+    ('fb703a0b-9593-4282-bdfc-942131f23182'::uuid),
+    ('2c61386d-0f37-48bb-a247-9cb5927d8974'::uuid)
+) AS account(id);
+
+INSERT INTO chat_message(chat_room_id, account_id, content)
+SELECT chat_room.id, account.id, 'content0'
+FROM chat_room
+CROSS JOIN ( 
+    VALUES 
+    ('65c13016-05f5-449b-9472-7afca5de2d03'::uuid), 
+    ('fb703a0b-9593-4282-bdfc-942131f23182'::uuid),
+    ('2c61386d-0f37-48bb-a247-9cb5927d8974'::uuid)
+) AS account(id);
+
+
+
